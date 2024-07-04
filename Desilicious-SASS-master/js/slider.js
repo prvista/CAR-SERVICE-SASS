574f702fd75b94380e46ba057be488bb799bb67b
KenSumang <vistapaulrainier@gmail.com>
2024-07-04 15:49:24 +0800

    first commit

---
+++ Desilicious-SASS-master/js/slider.js
@@ -0,0 +1,26 @@
+var slider = tns({
+    container: ".homeSlider__slider",
+    items: 1,
+    speed:700,
+    gutter: 100,
+    slideBy: "page",
+    autoplay: false,
+    navPosition: "bottom",
+    containerControls: "#controls",
+    prevButton: ".prev",
+    nextButton: ".next",
+    axis: "vertical",
+    mode: "gallery",
+
+    responsive: {
+        1250: {
+            items: 1,
+        },
+
+        200: {
+            items:1,
+        },
+    }
+});
+
+
